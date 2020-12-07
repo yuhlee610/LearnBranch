10d5f0d8cbfd43dee51251bd36d9a362329c1759
DucHuy <letranduchuy41@gmail.com>
2020-12-07 22:16:23 +0700

    yuhlee 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Write by master - 1");
+            Console.WriteLine("Write by yuhlee - 1");
             Console.ReadKey();
         }
        
