72a15be5eed084ffc833b622816a735cc07f1526
DucHuy <letranduchuy41@gmail.com>
2020-12-07 20:21:47 +0700

    m

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Write by master");
-            Console.WriteLine("------------");
             Console.ReadKey();
         }
         
