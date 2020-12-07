c173edf7c019e081023420bf4f3258a7a2b1690a
DucHuy <letranduchuy41@gmail.com>
2020-12-07 21:48:20 +0700

    up 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Write by duchuy - 1");
-            Console.WriteLine("Write by duchuy - 1");
             Console.ReadKey();
         }
        
